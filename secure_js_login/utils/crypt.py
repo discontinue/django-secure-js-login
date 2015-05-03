8da5e57c55b1d2904ddc402d02fe4ee81dfd288c
JensDiemer <git@jensdiemer.de>
2015-05-03 17:14:26 +0200

    WIP: update crypt - all DocTests worked.

--- secure_js_login/utils/crypt.py
+++ secure_js_login/utils/crypt.py
@@ -10,6 +10,20 @@
 
     unittest: ./dev_scripts/unittests/unittest_crypt.py
 
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
+    >>> encrypted_with_hash = encrypt(txt="foo", key="bar")
+    >>> encrypted_with_hash
+    'sha1$DEBUG_123456$197987f5ca3fd97da45acac3541e3464198a1cee$BA4d'
+
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
+    >>> decrypt(encrypted_with_hash, key="bar")
+    'foo'
+
+    >>> decrypt('sha1$DEBUG_123456$197987f5ca3fd97da45aXXc3541e3464198a1cee$BA4d', key="bar")
+    Traceback (most recent call last):
+        ...
+    SaltHashError: salt-sha1hash compare failed.
+
     :copyleft: 2007-2015 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
@@ -23,6 +37,7 @@ import random
 import re
 import sys
 import time
+import binascii
 
 if __name__ == "__main__":
     print("Local DocTest...")
@@ -31,11 +46,12 @@ if __name__ == "__main__":
 else:
     from django.conf import settings
 
+from django.utils import six
 from django.utils.encoding import force_bytes, force_text
 
 
-# Warning: Debug must always be False in productiv environment!
-#DEBUG = True
+# Warning: Debug must always be False in productive environment!
+# DEBUG = True
 DEBUG = False
 if DEBUG:
     import warnings
@@ -66,6 +82,7 @@ SHA1_RE = re.compile(r'[a-f0-9]{40}$')
 
 
 def hash_hexdigest(txt):
+    assert isinstance(txt, six.text_type), "txt: %s is not text type!" % repr(txt)
     return hashlib.sha1(force_bytes(txt)).hexdigest()
 
 
@@ -86,7 +103,7 @@ def validate_sha_value(sha_value):
     >>> validate_sha_value("790f2ebcb902c966fb0e232515ec1319dc9118af")
     True
     """
-    if not isinstance(sha_value, str):
+    if not isinstance(sha_value, six.text_type):
         return False
 
     if SHA1_RE.match(sha_value):
@@ -104,49 +121,68 @@ def validate_sha_value(sha_value):
 #    else:
 #        return True
 
-#______________________________________________________________________________
 
-
-def get_new_seed(can_debug=True):
+class SeedGenerator(object):
     """
-    Generate a new, random seed value.
+    Generate a new, random seed values.
 
-    >>> get_new_seed() # DEBUG is True in DocTest!
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
+    >>> seed_generator()
     'DEBUG_1234567890'
-    >>> seed = get_new_seed(can_debug=False)
-    >>> assert seed != 'DEBUG', "seed is: %s" % seed
-    >>> assert len(seed) == HASH_LEN, "Wrong length: %s" % len(seed)
+
+    try to check if every new seed is unique:
+    >>> seed_generator.DEBUG=False
+    >>> seeds = ['DEBUG_1234567890']
+    >>> for _ in range(10):
+    ...     seed = seed_generator()
+    ...     assert len(seed) == HASH_LEN, "Wrong length: %s" % len(seed)
+    ...     assert seed not in seeds
+    ...     seeds.append(seed)
+    >>> len(seeds)
+    11
     """
-    if can_debug and DEBUG:
-        seed = "DEBUG_1234567890"
-    else:
+    DEBUG=False
+    def __call__(self):
+        if self.DEBUG:
+            return "DEBUG_1234567890"
+
         raw_seed = "%s%s%s%s" % (
             random.randint(0, sys.maxsize - 1), os.getpid(), time.time(),
             settings.SECRET_KEY
         )
-        seed = hash_hexdigest(raw_seed)
+        return hash_hexdigest(raw_seed)
 
-    return seed
+seed_generator = SeedGenerator()
 
 
-def get_new_salt(can_debug=True):
+def get_new_salt():
     """
     Generate a new, random salt value.
 
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
     >>> get_new_salt() # DEBUG is True in DocTest!
     'DEBUG_123456'
-    >>> salt = get_new_salt(can_debug=False)
-    >>> assert salt != 'DEBUG_1234567890', "salt is: %s" % salt
-    >>> assert len(salt) == SALT_LEN, "Wrong length: %s" % len(salt)
+
+    try to check if every new salt is unique:
+    >>> seed_generator.DEBUG=False
+    >>> salts = ['DEBUG_123456']
+    >>> for _ in range(10):
+    ...     salt = get_new_salt()
+    ...     assert len(salt) == SALT_LEN, "Wrong length: %s" % len(salt)
+    ...     assert salt not in salts
+    ...     salts.append(salt)
+    >>> len(salts)
+    11
     """
-    seed = get_new_seed(can_debug)
+    seed = seed_generator()
     return seed[:SALT_LEN]
+
 
 def get_pseudo_salt(*args):
     """
     generate a pseudo salt (used, if user is wrong)
     """
-    temp = "".join([repr(arg) for arg in args])
+    temp = "".join([arg for arg in args])
     return hash_hexdigest(temp)[:SALT_LEN]
 
 
@@ -160,8 +196,9 @@ def make_hash(txt, salt):
     >>> make_hash(txt="test", salt='DEBUG')
     '790f2ebcb902c966fb0e232515ec1319dc9118af'
     """
-    txt = force_bytes(txt)
-    salt = force_bytes(salt)
+    assert isinstance(txt, six.text_type), "txt: %s is not text type!" % repr(txt)
+    assert isinstance(salt, six.text_type), "salt: %s is not text type!" % repr(salt)
+
     sha1hash = hash_hexdigest(salt + txt)
     return sha1hash
 
@@ -171,11 +208,26 @@ def get_salt_and_hash(txt):
     Generate a hast value with a random salt
     returned salt and sha1hash as a tuple
 
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
     >>> get_salt_and_hash("test")
     ('sha1', 'DEBUG_123456', '9f5ee85f5c91adb5741d8f93483386989d5d49ae')
+
+    try to check if every new salt/hash is unique:
+    >>> salts = ['DEBUG_123456']
+    >>> hashes = ['9f5ee85f5c91adb5741d8f93483386989d5d49ae']
+    >>> seed_generator.DEBUG=False
+    >>> for _ in range(10):
+    ...     hash_type, salt, hash = get_salt_and_hash("test")
+    ...     assert len(salt) == SALT_LEN, "Wrong length: %s" % len(salt)
+    ...     assert len(hash) == HASH_LEN, "Wrong length: %s" % len(salt)
+    ...     assert salt not in salts
+    ...     salts.append(salt)
+    ...     assert hash not in hashes
+    ...     hashes.append(hash)
+    >>> len(salts), len(hashes)
+    (11, 11)
     """
-    if not isinstance(txt, str):
-        raise SaltHashError("Only string allowed!")
+    assert isinstance(txt, six.text_type), "txt: %s is not text type!" % repr(txt)
 
     salt = get_new_salt()
     sha1hash = make_hash(txt, salt)
@@ -188,9 +240,22 @@ def make_salt_hash(txt):
     make from the given string a hash with a salt value
     returned one string back
 
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
     >>> make_salt_hash("test")
     'sha1$DEBUG_123456$9f5ee85f5c91adb5741d8f93483386989d5d49ae'
+
+    try to check if every new salt/hash is unique:
+    >>> seed_generator.DEBUG=False
+    >>> salt_hashes = ['sha1$DEBUG_123456$9f5ee85f5c91adb5741d8f93483386989d5d49ae']
+    >>> for _ in range(10):
+    ...     salt_hash = make_salt_hash("test")
+    ...     assert len(salt_hash) == SALT_HASH_LEN, "Wrong length: %s" % len(salt)
+    ...     assert salt_hash not in salt_hashes
+    ...     salt_hashes.append(salt_hash)
+    >>> len(salt_hashes)
+    11
     """
+    assert isinstance(txt, six.text_type), "txt: %s is not text type!" % repr(txt)
     salt_hash = "$".join(get_salt_and_hash(txt))
     return salt_hash
 
@@ -202,14 +267,15 @@ def check_salt_hash(txt, salt_hash):
     TODO: Should we used the django function for this?
         Look at: django.contrib.auth.models.check_password
 
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
     >>> salt_hash = make_salt_hash("test")
     >>> salt_hash
     'sha1$DEBUG_123456$9f5ee85f5c91adb5741d8f93483386989d5d49ae'
     >>> check_salt_hash("test", salt_hash)
     True
     """
-#    if not (isinstance(txt, str) and isinstance(salt_hash, str)):
-#        raise SaltHashError("Only string allowed!")
+    assert isinstance(txt, six.text_type), "txt: %s is not text type!" % repr(txt)
+    assert isinstance(salt_hash, six.text_type), "salt_hash: %s is not text type!" % repr(salt_hash)
 
     if len(salt_hash) not in (SALT_HASH_LEN, OLD_SALT_HASH_LEN):
         raise SaltHashError("Wrong salt-sha1hash length.")
@@ -223,7 +289,7 @@ def check_salt_hash(txt, salt_hash):
         raise SaltHashError("Unsupported sha1hash method.")
 
     test_hash = make_hash(txt, salt)
-#    raise
+
     if sha1hash != test_hash:
         msg = "salt-sha1hash compare failed."
         if DEBUG:
@@ -249,141 +315,138 @@ def salt_hash_to_dict(salt_hash):
     }
 
 
-#______________________________________________________________________________
 
 class CryptLengthError(AssertionError):
     pass
 
 
-def crypt(txt, key):
+class XorCryptor(object):
     """
     XOR ciphering
-    >txt< and >key< should be unicode.
 
-    >>> crypt("1234", "ABCD")
-    'pppp'
+    >>> crypt(b"1234", b"ABCD")
+    b'pppp'
     """
-    if len(txt) != len(key):
-        raise CryptLengthError("XOR cipher error: %r and %r must have the same length!" % (txt, key))
+    def __call__(self, txt, key):
+        assert isinstance(txt, six.binary_type), "txt: %s is not binary type!" % repr(txt)
+        assert isinstance(key, six.binary_type), "key: %s is not binary type!" % repr(key)
 
-    crypted = [chr(ord(t) ^ ord(k)) for t, k in zip(txt, key)]
-    return force_text("".join(crypted))
+        if len(txt) != len(key):
+            raise CryptLengthError("XOR cipher error: %r and %r must have the same length!" % (txt, key))
+
+        if six.PY2:
+            crypted = "".join([chr(ord(t) ^ ord(k)) for t, k in zip(txt, key)])
+        else:
+            crypted = [(t ^ k) for t, k in zip(txt, key)]
+            crypted = bytes(crypted)
+        return crypted
+
+crypt=XorCryptor()
 
 
-def encrypt(txt, key, use_base64=True, can_debug=True):
+def encrypt(txt, key):
     """
     XOR ciphering with a SHA salt-hash checksum
 
-    >>> encrypt("1234", "ABCD") # DEBUG is True in DocTest!
-    'crypt 1234 with ABCD'
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
+    >>> make_salt_hash("1234")
+    'sha1$DEBUG_123456$7a3a52f5e751461ed74f09d18010dcd6e3acf653'
 
-    >>> encrypt("1234", "ABCD", can_debug=False)
-    'sha1$DEBUG_123456$91ca222581d9b8f61934d7bf25fb3625141cda91cHBwcA=='
-
-    >>> encrypt("1234", "ABCD", use_base64=False, can_debug=False)
-    'sha1$DEBUG_123456$91ca222581d9b8f61934d7bf25fb3625141cda91pppp'
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
+    >>> encrypt("1234", "ABCD")
+    'sha1$DEBUG_123456$7a3a52f5e751461ed74f09d18010dcd6e3acf653$cHBwcA=='
     """
-    if not (isinstance(txt, str) and isinstance(key, str)):
-        raise UnicodeError("Only unicode allowed!")
+    assert isinstance(txt, six.text_type), "txt: %s is not text type!" % repr(txt)
+    assert isinstance(key, six.text_type), "key: %s is not text type!" % repr(key)
 
-    if can_debug and DEBUG:
-        return "crypt %s with %s" % (txt, key)
+    if len(txt) != len(key):
+        raise CryptLengthError("encrypt error: %r and %r must have the same length!" % (txt, key))
 
-    salt_hash = make_salt_hash(repr(txt))
-    salt_hash = force_text(salt_hash)
+    salt_hash = make_salt_hash(txt)
+
+    txt=force_bytes(txt)
+    key=force_bytes(key)
 
     crypted = crypt(txt, key)
-    if use_base64 == True:
-        crypted = force_text(base64.b64encode(force_bytes(crypted)))
-    return salt_hash + crypted
+    crypted = base64.b64encode(crypted)
+
+    crypted = six.text_type(crypted, "ascii")
+    return "%s$%s" % (salt_hash, crypted)
 
 
-def decrypt(crypted, key, use_base64=True, can_debug=True):
+def decrypt(txt, key):
     """
     1. Decrypt a XOR crypted String.
-    2. Compare the inserted sSHA salt-hash checksum.
+    2. Compare the inserted SHA salt-hash checksum.
 
-    >>> decrypt('crypt 1234 with ABCD', "ABCD") # DEBUG is True in DocTest!
-    '1234'
-
-    >>> crypted = encrypt("1234", "ABCD", can_debug=False)
-    >>> crypted
-    'sha1$DEBUG_123456$91ca222581d9b8f61934d7bf25fb3625141cda91cHBwcA=='
-    >>> decrypt(crypted, "ABCD", can_debug=False)
-    '1234'
-
-    >>> decrypt('sha1$DEBUG$b323f546665b1f034742630133d1b489480a24e2cHBwcA==', "ABCD", can_debug=False)
-    '1234'
-
-    >>> crypted = encrypt("1234", "ABCD", use_base64=False, can_debug=False)
-    >>> decrypt(crypted, "ABCD", use_base64=False, can_debug=False)
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
+    >>> decrypt("sha1$DEBUG_123456$7a3a52f5e751461ed74f09d18010dcd6e3acf653$cHBwcA==", "ABCD")
     '1234'
     """
-    crypted = force_text(crypted)
-    key = force_text(key)
+    assert isinstance(txt, six.text_type), "txt: %s is not text type!" % repr(txt)
+    assert isinstance(key, six.text_type), "key: %s is not text type!" % repr(key)
 
-    if can_debug and DEBUG:
-        txt, _, key2 = crypted.split(" ", 3)[1:]
-        assert key == key2, "key: %s != key2: %s" % (key, key2)
-        return txt
+    salt_hash, crypted = txt.rsplit("$",1)
 
-    salt_hash = force_text(crypted[:SALT_HASH_LEN])
-    crypted1 = crypted[SALT_HASH_LEN:]
-    if use_base64 == True:
-        crypted1 = base64.b64decode(crypted1)
-        crypted1 = force_text(crypted1)
+    if not seed_generator.DEBUG and len(salt_hash)!=SALT_HASH_LEN:
+        raise CryptLengthError(
+            "encrypt error: Salt-hash %s with length %i must be length %i!" % (
+                repr(salt_hash), len(salt_hash), SALT_HASH_LEN
+            )
+        )
 
     try:
-        decrypted = crypt(crypted1, key)
-    except CryptLengthError:
-        # Try with the OLD_SALT_HASH_LEN
-        salt_hash = force_text(crypted[:OLD_SALT_HASH_LEN])
-        crypted2 = crypted[OLD_SALT_HASH_LEN:]
-        if use_base64 == True:
-            crypted2 = base64.b64decode(crypted2)
-            crypted2 = force_text(crypted2)
-        decrypted = crypt(crypted2, key)
+        crypted = base64.b64decode(crypted, validate=True)
+    except binascii.Error as err:
+        raise CryptLengthError("b64decode error: %s with data: %s" % (err, repr(crypted)))
+
+    if len(crypted) != len(key):
+        raise CryptLengthError("encrypt error: %r and %r must have the same length!" % (crypted, key))
+
+    key=force_bytes(key)
+    decrypted = crypt(crypted, key)
+
+    decrypted = force_text(decrypted)
 
     # raised a SaltHashError() if the checksum is wrong:
-    check_salt_hash(repr(decrypted), salt_hash)
+    check_salt_hash(decrypted, salt_hash)
 
     return decrypted
 
 
-#______________________________________________________________________________
 
-def django_to_sha_checksum(django_salt_hash):
-    """
-    Create a JS-SHA-Checksum from the django user password.
-    (For the unittest)
+# def django_to_sha_checksum(django_salt_hash):
+#     """
+#     Create a JS-SHA-Checksum from the django user password.
+#     (For the unittest)
+#
+#     The >django_salt_hash< is:
+#         user = User.objects.get(...)
+#         django_salt_hash = user.password
+#
+#     >>> django_to_sha_checksum("sha1$DEBUG$50b412a7ef09f4035f2daca882a1f8bfbe263b62")
+#     ('DEBUG', 'crypt 50b412a7ef09f4035f2d with aca882a1f8bfbe263b62')
+#     """
+#     hash_typ, salt, hash_value = django_salt_hash.split("$")
+#     assert hash_typ == "sha1", "hash_value typ not supported!"
+#     assert len(hash_value) == HASH_LEN, "Wrong hash_value length! (Not a SHA1 hash_value?)"
+#
+#     # Split the SHA1-Hash in two pieces
+#     sha_a = hash_value[:HALF_HASH_LEN]
+#     sha_b = hash_value[HALF_HASH_LEN:]
+#
+#     sha_checksum = encrypt(txt=sha_a, key=sha_b)
+#
+#     return salt, sha_checksum
 
-    The >django_salt_hash< is:
-        user = User.objects.get(...)
-        django_salt_hash = user.password
-
-    >>> django_to_sha_checksum("sha1$DEBUG$50b412a7ef09f4035f2daca882a1f8bfbe263b62")
-    ('DEBUG', 'crypt 50b412a7ef09f4035f2d with aca882a1f8bfbe263b62')
-    """
-    hash_typ, salt, hash_value = django_salt_hash.split("$")
-    assert hash_typ == "sha1", "hash_value typ not supported!"
-    assert len(hash_value) == HASH_LEN, "Wrong hash_value length! (Not a SHA1 hash_value?)"
-
-    # Split the SHA1-Hash in two pieces
-    sha_a = hash_value[:HALF_HASH_LEN]
-    sha_b = hash_value[HALF_HASH_LEN:]
-
-    sha_a = force_text(sha_a)
-    sha_b = force_text(sha_b)
-    sha_checksum = encrypt(txt=sha_a, key=sha_b)
-
-    return salt, sha_checksum
 
 def make_sha_checksum2(raw_password):
     """
     Create a SHA1-JS-Login checksum from a plaintext password.
 
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
     >>> make_sha_checksum2("test")
-    ('DEBUG_123456', 'crypt 9f5ee85f5c91adb5741d with 8f93483386989d5d49ae')
+    ('DEBUG_123456', 'sha1$DEBUG_123456$a25864a25dc950270a88853e26b027b3a59f953d$AQAMVlEABlUNVQAJWABXUQMNUAE=')
     """
     _, salt, hash_value = get_salt_and_hash(raw_password)
 
@@ -393,15 +456,34 @@ def make_sha_checksum(hash_value):
     """
     Made the needed sha_checksum for the SHA1-JS-Login.
 
-    >>> make_sha_checksum("50b412a7ef09f4035f2daca882a1f8bfbe263b62")
-    'crypt 50b412a7ef09f4035f2d with aca882a1f8bfbe263b62'
+    >>> hash = hash_hexdigest("foobar")
+    >>> hash
+    '8843d7f92416211de9ebb963ff4ce28125932878'
+
+    # Split the hash:
+    >>> sha_a = "8843d7f92416211de9eb"
+    >>> sha_b = "b963ff4ce28125932878"
+
+    # Make the SHA-Checksum:
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
+    >>> sha_checksum = make_sha_checksum(hash)
+    >>> sha_checksum
+    'sha1$DEBUG_123456$7213c1ffd40d305496b98c493adffd04e2a312c4$WgECAAJRUlpXBgkHAAQIV1cBUlo='
+
+    # Test the created checksum:
+    >>> test = decrypt(sha_checksum, key=sha_b)
+    >>> test
+    '8843d7f92416211de9eb'
+    >>> test == sha_a
+    True
     """
+    assert len(hash_value)==HASH_LEN
+
     # Split the SHA1-Hash in two pieces
     sha_a = hash_value[:HALF_HASH_LEN]
     sha_b = hash_value[HALF_HASH_LEN:]
+    # print(sha_a, sha_b)
 
-    sha_a = force_text(sha_a)
-    sha_b = force_text(sha_b)
     sha_checksum = encrypt(txt=sha_a, key=sha_b)
     return sha_checksum
 
@@ -419,9 +501,10 @@ def check_js_sha_checksum(challenge, sha_a, sha_b, sha_checksum, loop_count, cno
     >>> hash_value = make_hash(password, salt1)
     >>> hash_value
     'f893fc3ebdfd886836822161b6bc2ccac955e014'
+    >>> seed_generator.DEBUG=True # Generate always the same seed for tests
     >>> sha_checksum = make_sha_checksum(hash_value)
     >>> sha_checksum
-    'crypt f893fc3ebdfd88683682 with 2161b6bc2ccac955e014'
+    'sha1$DEBUG_123456$b323acd534000e3e35a690a5648fe73db783ac70$VAkPAgRVUQZQBwUFWwEDDVYGCQY='
     >>>
     >>> sha_a = hash_value[:HALF_HASH_LEN]
     >>> sha_a
@@ -459,7 +542,7 @@ def check_js_sha_checksum(challenge, sha_a, sha_b, sha_checksum, loop_count, cno
 
 
 if __name__ == "__main__":
-    DEBUG = True
+    # print(crypt(b"1234", b"ABCD"))
 
     import doctest
     print(doctest.testmod(
