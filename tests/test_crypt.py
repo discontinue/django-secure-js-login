590a1897857dbc660e122f12d01f6f182fce1bcf
JensDiemer <git@jensdiemer.de>
2015-05-03 20:16:40 +0200

    WIP: unitests

---
+++ tests/test_crypt.py
@@ -0,0 +1,33 @@
+
+import unittest
+
+from secure_js_login.utils import crypt
+
+
+class TestCrypt(unittest.TestCase):
+    # def setUp(self):
+    #     crypt.seed_generator.DEBUG=True
+    #
+    # def tearDown(self):
+    #     crypt.seed_generator.DEBUG=False
+        
+    def test_encrypt(self):
+        crypt.seed_generator.DEBUG=True # Generate always the same seed for tests
+        self.assertEqual(
+            crypt.encrypt(txt="foo", key="bar"),
+            'sha1$DEBUG_123456$197987f5ca3fd97da45acac3541e3464198a1cee$BA4d'
+        )
+
+    def test_decrypted(self):
+        crypt.seed_generator.DEBUG=True # Generate always the same seed for tests
+        self.assertEqual(
+            crypt.decrypt('sha1$DEBUG_123456$197987f5ca3fd97da45acac3541e3464198a1cee$BA4d', key="bar"),
+            'foo'
+        )
+
+    def test_decrypt_wrong_hash(self):
+        self.assertRaises(crypt.SaltHashError,
+            crypt.decrypt,
+            'sha1$DEBUG_123456$197987f5ca3fd97da45aXXc3541e3464198a1cee$BA4d',
+            key="bar"
+        )
