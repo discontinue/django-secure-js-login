0c47a206ae6a7a6759dec1910d34038f0afd4771
JensDiemer <git@jensdiemer.de>
2015-05-06 22:26:44 +0200

    increase default iteration after testing on a Raspberry Pi 1

--- secure_js_login/settings.py
+++ secure_js_login/settings.py
@@ -13,14 +13,14 @@ MIN_PAUSE = getattr(settings, "MIN_PAUSE", 5)
 USE_HONYPOT = getattr(settings, "USE_HONYPOT", False)
 
 # Number of loops in PBKDF2 for hash based key stretching.
-# TODO: Tryout how fast e.g. a Raspberry Pi is!
-# Notes:
-# * Higher count increase the security, but causes more CPU load on client and server.
-#   * Use higher values carefully, because of showiness of the JavaScript Implementation!
+# Higher count increase the security, but causes more CPU load on client and server.
+# The compute time of together 2000 iterations on a Raspberry Pi 1 and firefox are acceptable.
+# django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher() used currently iterations = 24000
+# Note:
 #   * **ITERATIONS1** should be not changed if passwords already stored in database!
 #   * **ITERATIONS2** can be changed very time!
-ITERATIONS1 = getattr(settings, "ITERATIONS1", 100) # for storing a part of the password in the database
-ITERATIONS2 = getattr(settings, "ITERATIONS2", 50) # for generate on-the-fly the server challenge
+ITERATIONS1 = getattr(settings, "ITERATIONS1", 1000) # for storing a part of the password in the database
+ITERATIONS2 = getattr(settings, "ITERATIONS2", 1000) # for generate on-the-fly the server challenge
 
 # Length of the generated PBKDF2 hash in bytes:
 PBKDF2_BYTE_LENGTH = getattr(settings, "PBKDF2_BYTE_LENGTH", 32) # max length: 256 by model field!
@@ -29,7 +29,7 @@ PBKDF2_BYTE_LENGTH = getattr(settings, "PBKDF2_BYTE_LENGTH", 32) # max length: 2
 PBKDF2_SALT_LENGTH = getattr(settings, "PBKDF2_SALT_LENGTH", 12) # max length: 256 by model field!
 RANDOM_CHALLENGE_LENGTH = getattr(settings, "RANDOM_CHALLENGE_LENGTH", 24)
 
-# generated with SHA1 (Hex reprentation), so max length is: 40
+# generated with SHA1 (Hex representation), so max length is: 40
 CLIENT_NONCE_LENGTH = getattr(settings, "CLIENT_NONCE_LENGTH", 24)
 
 # Use https (secure http) for login forms?
