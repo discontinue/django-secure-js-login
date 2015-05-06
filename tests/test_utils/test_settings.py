29f192339dcca238e38bb82f378fda24b7111e62
JensDiemer <git@jensdiemer.de>
2015-05-06 17:56:33 +0200

    speed up tests

--- tests/test_utils/test_settings.py
+++ tests/test_utils/test_settings.py
@@ -4,13 +4,11 @@ from example_project.example_project.settings import *
 ROOT_URLCONF = 'example_project.example_project.urls'
 
 # For faster unittests:
-# ITERATIONS1 = 5
-# ITERATIONS2 = 10
+ITERATIONS1 = 5
+ITERATIONS2 = 10
 
-# PBKDF2_BYTE_LENGTH = 24
+PBKDF2_BYTE_LENGTH = 12
 
-# PBKDF2_SALT_LENGTH = 5
-# RANDOM_CHALLENGE_LENGTH = 6
-# CLIENT_NONCE_LENGTH = 8
-
-DEBUG=True
+PBKDF2_SALT_LENGTH = 5
+RANDOM_CHALLENGE_LENGTH = 6
+CLIENT_NONCE_LENGTH = 8
