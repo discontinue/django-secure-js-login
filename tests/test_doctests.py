546f79a59173555b947a888dea9ab12e836098f3
JensDiemer <git@jensdiemer.de>
2015-05-12 11:02:27 +0200

    fix py2

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -1,3 +1,16 @@
+# coding: utf-8
+
+"""
+    Secure JavaScript Login
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyleft: 2015 by the secure-js-login team, see AUTHORS for more details.
+    :created: by JensDiemer.de
+    :license: GNU GPL v3 or above, see LICENSE for more details
+"""
+
+from __future__ import unicode_literals, print_function
+
 import os
 import unittest
 import doctest
