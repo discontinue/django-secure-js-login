1ccc39a6f20921907858e88af6b64958b15b8d94
JensDiemer <git@jensdiemer.de>
2015-07-26 20:28:56 +0200

    update "./setup.py publish" code

--- secure_js_login/__init__.py
+++ secure_js_login/__init__.py
@@ -1,13 +1,2 @@
 
-
-from __future__ import absolute_import, division, print_function
-
-
-__version__ = (0,2,0)
-
-VERSION_STRING = '.'.join(str(part) for part in __version__)
-
-
-
-if __name__ == "__main__":
-    print(VERSION_STRING)
+__version__ = "0.3.dev0"
--- setup.py
+++ setup.py
@@ -22,11 +22,51 @@ import sys
 
 from setuptools import setup, find_packages
 
-from secure_js_login import VERSION_STRING
+from secure_js_login import __version__
+
+
+PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+# convert README.creole on-the-fly to ReSt, see also:
+# https://github.com/jedie/python-creole/wiki/Use-In-Setup/
+check_readme="publish" in sys.argv or "check" in sys.argv or "register" in sys.argv or "sdist" in sys.argv or "--long-description" in sys.argv
+try:
+    from creole.setup_utils import get_long_description
+except ImportError as err:
+    if check_readme:
+        raise ImportError("%s - Please install python-creole >= v0.8 -  e.g.: pip install python-creole" % err)
+    long_description = None
+else:
+    if check_readme:
+        print("\nCheck creole2ReSt:")
+    long_description = get_long_description(PACKAGE_ROOT)
+    if check_readme:
+        print("OK")
 
 
 if "publish" in sys.argv:
+    """
+    Build and upload to PyPi, if...
+        ... __version__ doesn't contains "dev"
+        ... we are on git 'master' branch
+        ... git repository is 'clean' (no changed files)
+
+    Upload with "twine", git tag the current version and git push --tag
+
+    The cli arguments will be pass to 'twine'. So this is possible:
+     * Display 'twine' help page...: ./setup.py publish --help
+     * use testpypi................: ./setup.py publish --repository=test
+
+    TODO: Look at: https://github.com/zestsoftware/zest.releaser
+    """
+    # Imports here, so it's easier to copy&paste this complete code block ;)
+    import subprocess
+    import shutil
+
     try:
+        # Test if wheel is installed, otherwise the user will only see:
+        #   error: invalid command 'bdist_wheel'
         import wheel
     except ImportError as err:
         print("\nError: %s" % err)
@@ -35,31 +75,107 @@ if "publish" in sys.argv:
         print("    ~/your/env/$ source bin/activate")
         print("    ~/your/env/$ pip install wheel")
         sys.exit(-1)
-        
-    import subprocess
-    args = [sys.executable or "python", "setup.py", "sdist", "bdist_wheel", "upload"]
-    print("\nCall: %r\n" %  " ".join(args))
-    subprocess.call(args)
 
-    print("\nDon't forget to tag this version, e.g.:")
-    print("\tgit tag v%s" % VERSION_STRING)
-    print("\tgit push --tags")
-    sys.exit()
+    try:
+        import twine
+    except ImportError as err:
+        print("\nError: %s" % err)
+        print("\nMaybe https://pypi.python.org/pypi/twine is not installed or virtualenv not activated?!?")
+        print("e.g.:")
+        print("    ~/your/env/$ source bin/activate")
+        print("    ~/your/env/$ pip install twine")
+        sys.exit(-1)
 
+    def verbose_check_output(*args):
+        """ 'verbose' version of subprocess.check_output() """
+        call_info = "Call: %r" % " ".join(args)
+        try:
+            output = subprocess.check_output(args, universal_newlines=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as err:
+            print("\n***ERROR:")
+            print(err.output)
+            raise
+        return call_info, output
 
-PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
+    def verbose_check_call(*args):
+        """ 'verbose' version of subprocess.check_call() """
+        print("\tCall: %r\n" % " ".join(args))
+        subprocess.check_call(args, universal_newlines=True)
 
+    if "dev" in __version__:
+        print("\nERROR: Version contains 'dev': v%s\n" % __version__)
+        sys.exit(-1)
 
-# convert creole to ReSt on-the-fly, see also:
-# https://code.google.com/p/python-creole/wiki/UseInSetup
-try:
-    from creole.setup_utils import get_long_description
-except ImportError as err:
-    if "check" in sys.argv or "register" in sys.argv or "sdist" in sys.argv or "--long-description" in sys.argv:
-        raise ImportError("%s - Please install python-creole >= v0.8 - e.g.: pip install python-creole" % err)
-    long_description = None
-else:
-    long_description = get_long_description(PACKAGE_ROOT)
+    print("\nCheck if we are on 'master' branch:")
+    call_info, output = verbose_check_output("git", "branch", "--no-color")
+    print("\t%s" % call_info)
+    if "* master" in output:
+        print("OK")
+    else:
+        print("\nNOTE: It seems you are not on 'master':")
+        print(output)
+        if input("\nPublish anyhow? (Y/N)").lower() not in ("y", "j"):
+            print("Bye.")
+            sys.exit(-1)
+
+    print("\ncheck if if git repro is clean:")
+    call_info, output = verbose_check_output("git", "status", "--porcelain")
+    print("\t%s" % call_info)
+    if output == "":
+        print("OK")
+    else:
+        print("\n *** ERROR: git repro not clean:")
+        print(output)
+        sys.exit(-1)
+
+    print("\ncheck if pull is needed")
+    verbose_check_call("git", "fetch", "--all")
+    call_info, output = verbose_check_output("git", "log", "HEAD..origin/master", "--oneline")
+    print("\t%s" % call_info)
+    if output == "":
+        print("OK")
+    else:
+        print("\n *** ERROR: git repro is not up-to-date:")
+        print(output)
+        sys.exit(-1)
+    verbose_check_call("git", "push")
+
+    print("\nCleanup old builds:")
+    def rmtree(path):
+        path = os.path.abspath(path)
+        if os.path.isdir(path):
+            print("\tremove tree:", path)
+            shutil.rmtree(path)
+    rmtree("./dist")
+    rmtree("./build")
+
+    print("\nbuild but don't upload...")
+    log_filename="build.log"
+    with open(log_filename, "a") as log:
+        call_info, output = verbose_check_output(
+            sys.executable or "python",
+            "setup.py", "sdist", "bdist_wheel", "bdist_egg"
+        )
+        print("\t%s" % call_info)
+        log.write(call_info)
+        log.write(output)
+    print("Build output is in log file: %r" % log_filename)
+
+    print("\ngit tag version (will raise a error of tag already exists)")
+    verbose_check_call("git", "tag", "v%s" % __version__)
+
+    print("\nUpload with twine:")
+    twine_args = sys.argv[1:]
+    twine_args.remove("publish")
+    twine_args.insert(1, "dist/*")
+    print("\ttwine upload command args: %r" % " ".join(twine_args))
+    from twine.commands.upload import main as twine_upload
+    twine_upload(twine_args)
+
+    print("\ngit push tag to server")
+    verbose_check_call("git", "push", "--tags")
+
+    sys.exit(0)
 
 
 def get_authors():
@@ -71,10 +187,9 @@ def get_authors():
     return authors
 
 
-
 setup_info = dict(
     name='django-secure-js-login',
-    version=VERSION_STRING,
+    version=__version__,
     description='JavaScript Challenge-handshake authentication django app',
     long_description=long_description,
     author=get_authors(),
