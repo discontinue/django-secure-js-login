d73a2d082903bdf4f8f31b37f1a5feff896ef8a4
JensDiemer <git@jensdiemer.de>
2015-07-26 22:27:29 +0200

    release as v0.3.alpha0

--- setup.py
+++ setup.py
@@ -210,8 +210,8 @@ setup(
     classifiers=[
        # 'Development Status :: 1 - Planning',
        # 'Development Status :: 2 - Pre-Alpha',
-       # 'Development Status :: 3 - Alpha',
-       "Development Status :: 4 - Beta",
+       'Development Status :: 3 - Alpha',
+       # "Development Status :: 4 - Beta",
        #  "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
